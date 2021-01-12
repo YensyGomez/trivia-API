3c245d7502f82f0a33cbfa246cfd8c88d7457801
YensyGomez <yensy@sirius.utp.edu.co>
2021-01-12 11:49:12 +0100

    create get reguest categories and question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,16 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page -1)* QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -17,7 +27,6 @@ def create_app(test_config=None):
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
   CORS(app)
-
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
@@ -35,14 +44,13 @@ def create_app(test_config=None):
   @app.route('/categories')
   def retrieve_categories():
     categories = Category.query.order_by(Category.type).all()
-    if len(categories) < 0:
+    if len(categories) == 0:
       abort(404)
     return jsonify({
-            'success': True,
-            'categories': {category.id: category.type for category in categories}
-        })
+      'success': True,
+      'categories': {category.id: category.type for category in categories}
+      })
     
-
 
   '''
   @TODO: 
@@ -56,6 +64,23 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions')
+  def get_questions():
+    selection = Question.query.order_by(Question.id).all()
+    current_questions = paginate_questions(request, selection)
+
+    categories = Category.query.all()
+
+    if len(current_questions) < 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'total_questions': len(selection),
+      'categories': [category.type for category in categories],
+      'current_category': None
+    })
 
   '''
   @TODO: 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,34 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+
+    def test_get_questions(self):
+       
+      'success': True,
+      'questions': current_questions,
+      'total_questions': len(selection),
+      'categories': [category.type for category in categories],
+      'current_category': None
+
+
+
 
 
 # Make the tests conveniently executable
