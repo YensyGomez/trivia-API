03ea71f22f82095832f4fbcc5c98e307f0dc4525
YensyGomez <yensy@sirius.utp.edu.co>
2021-01-19 11:45:50 +0100

    error handler 422 play quiz

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -196,25 +196,35 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+
   @app.route('/quizzes', methods=['POST'])
   def play_quiz():
+    try:
 
-    body = request.get_json()
-    category = body.get('quiz_category')
-    before_questions= body.get('previous_questions')
+        body = request.get_json()
 
-    if category['type'] == 'click':
-        new_question = Question.query.filter(
-            Question.id.notin_((before_questions))).first()
+        if not ('quiz_category' in body and 'previous_questions' in body):
+            abort(422)
 
-    else:
-        new_question = Question.query.filter_by(
-            category=category['id']).filter(Question.id.notin_((before_questions))).first()
+        category = body.get('quiz_category')
+        previous_questions = body.get('previous_questions')
 
-    return jsonify({
-        'success': True,
-        'question': new_question.format() if new_question else None,
-    })
+        if category['type'] == 'click':
+            available_questions = Question.query.filter(
+                Question.id.notin_((previous_questions))).all()
+        else:
+            available_questions = Question.query.filter_by(
+                category=category['id']).filter(Question.id.notin_((previous_questions))).all()
+
+        new_question = available_questions[random.randrange(
+            0, len(available_questions))].format() if len(available_questions) > 0 else None
+
+        return jsonify({
+            'success': True,
+            'question': new_question
+        })
+    except:
+        abort(422)
   '''
   @TODO: 
   Create error handlers for all expected errors 
@@ -242,7 +252,7 @@ def create_app(test_config=None):
         "error": 400,
         "message": "bad request"
     }), 400
-    
+
   return app
 
     
